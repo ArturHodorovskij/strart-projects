6fe62d2460369e31a79fef836f2f9c694f83cdbc
ArturHodorovskij <tjarturtj@gmail.com>
2024-10-07 20:15:56 +0300

    Correct name of variable

--- src/Converter.kt
+++ src/Converter.kt
@@ -43,9 +43,9 @@ fun converterTemperature() {
     while (true) {
         print("Введите температуру в градусах цельсия: ")
         val readTemp = readln().toDoubleOrNull() ?: continue
-        val faringate = readTemp * 1.8 + 32
+        val fahrenheit = readTemp * 1.8 + 32
         val kelvin = readTemp + 273.15
-        println("Температура в фарингейтах: $faringate")
+        println("Температура в фарингейтах: $fahrenheit")
         println("Температура в кельвинах: $kelvin")
         break
     }
